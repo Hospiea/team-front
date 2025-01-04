58e6ee1884c7d3a5efca7f5926ea1a80cf6c8850
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-01-05 05:52:11 +0900

    스타일 수정 및 사용자 데이터 전송 기능 추가, 사용자 DTO 타입 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import Image from "next/image";
 import styles from "./style.module.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const init = async () => {
+    try{
+      const response = await axios.post("http://localhost:5050/user", {
+        name: "Hello",
+        email: "asdf",
+        gender: "MALE",
+        age: 24,
+      });
+      console.log(response.data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div className={styles.container}>
       asdf
---
+++ src/types/dto.ts
@@ -0,0 +1,6 @@
+export type UserDto = {
+  name: string;
+  email: string;
+  gender: "MALE" | "FEMALE";
+  age: number;
+}
