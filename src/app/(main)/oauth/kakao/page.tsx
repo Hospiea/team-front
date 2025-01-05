644447c13726f553963a67d181a007e1d1655475
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-01-05 12:40:12 +0900

    카카오 로그인 기능 구현 및 관련 DTO 타입 정의

--- src/app/(main)/oauth/kakao/page.tsx
+++ src/app/(main)/oauth/kakao/page.tsx
@@ -1,13 +1,48 @@
-
+"use client";
+import { BACKEND_URL, KAKAO_REDIRECT_URI, KAKAO_REST_API_KEY } from "@/config/config";
+import { LoginRequest } from "@/types/dto";
+import axios from "axios";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const KakaoLoginPage = () => {
+  const params = useSearchParams();
+  
+  const handleLogin = async (code: string) => {
+    try{
+      const response = await axios.post(`https://kauth.kakao.com/oauth/token`, {
+        grant_type: "authorization_code",
+        client_id: `${KAKAO_REST_API_KEY}`,
+        redirect_uri: `${KAKAO_REDIRECT_URI}`,
+        code: code,
+      }, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        }
+      });
+      if(response.data) {
+        const loginRequest: LoginRequest = {
+          access_token: response.data.access_token,
+          refresh_token: response.data.refresh_token,
+        }
+        const result = await axios.post(`${BACKEND_URL}/user`, loginRequest);
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
 
+  useEffect(() => {
+    if (params) {
+      handleLogin(params.get("code") as string);
+    }
+  }, [params]);
 
   return (
-    <div>
-      <button>카카오 로그인</button>
+    <div className="flex h-full items-center justify-center">
+      <h1>로그인 중입니다...</h1>
     </div>
   );
-}
+};
 
 export default KakaoLoginPage;
--- src/types/dto.ts
+++ src/types/dto.ts
@@ -1,6 +1,18 @@
-export type UserDto = {
-  name: string;
-  email: string;
-  gender: "MALE" | "FEMALE";
-  age: number;
+export interface KakaoResponse {
+	id: number;
+	connected_at: string;
 }
+
+export interface UserDto {
+	name: string;
+	email: string;
+	age: number;
+	gender?: string;
+}
+
+export interface LoginRequest {
+	user_info?: UserDto;
+	access_token: string;
+	refresh_token: string;
+}
+
