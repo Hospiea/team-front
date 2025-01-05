0751899383faa250580b0492e048dded02c819eb
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-01-05 12:43:31 +0900

    카카오 로그인 페이지에 Suspense 추가 및 코드 정리

--- src/app/(main)/oauth/kakao/page.tsx
+++ src/app/(main)/oauth/kakao/page.tsx
@@ -1,36 +1,44 @@
 "use client";
-import { BACKEND_URL, KAKAO_REDIRECT_URI, KAKAO_REST_API_KEY } from "@/config/config";
+import {
+  BACKEND_URL,
+  KAKAO_REDIRECT_URI,
+  KAKAO_REST_API_KEY,
+} from "@/config/config";
 import { LoginRequest } from "@/types/dto";
 import axios from "axios";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 const KakaoLoginPage = () => {
   const params = useSearchParams();
-  
+
   const handleLogin = async (code: string) => {
-    try{
-      const response = await axios.post(`https://kauth.kakao.com/oauth/token`, {
-        grant_type: "authorization_code",
-        client_id: `${KAKAO_REST_API_KEY}`,
-        redirect_uri: `${KAKAO_REDIRECT_URI}`,
-        code: code,
-      }, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    try {
+      const response = await axios.post(
+        `https://kauth.kakao.com/oauth/token`,
+        {
+          grant_type: "authorization_code",
+          client_id: `${KAKAO_REST_API_KEY}`,
+          redirect_uri: `${KAKAO_REDIRECT_URI}`,
+          code: code,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+          },
         }
-      });
-      if(response.data) {
+      );
+      if (response.data) {
         const loginRequest: LoginRequest = {
           access_token: response.data.access_token,
           refresh_token: response.data.refresh_token,
-        }
+        };
         const result = await axios.post(`${BACKEND_URL}/user`, loginRequest);
       }
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   useEffect(() => {
     if (params) {
@@ -39,9 +47,13 @@ const KakaoLoginPage = () => {
   }, [params]);
 
   return (
-    <div className="flex h-full items-center justify-center">
-      <h1>로그인 중입니다...</h1>
-    </div>
+    <Suspense
+      fallback={
+        <div className="flex h-full items-center justify-center">
+          <h1>로그인 중입니다...</h1>
+        </div>
+      }
+    ></Suspense>
   );
 };
 
